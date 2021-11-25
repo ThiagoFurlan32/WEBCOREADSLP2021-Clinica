4986b03cf44dd1ef0ad5ff9371717f56c18fa886
Lucas Gomes Torresilhas <2011550161@fema.edu.br>
2021-11-24 23:51:05 -0300

    Inicio da Consulta de Agrupamento de Dados de Várias Origens.

---
+++ Controllers/AgendamentoController1.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WEBCORELP2021.Models;
+using WEBCORELP2021.Models.Consultas;
+using WEBCORELP2021.Models.Dominio;
+
+namespace WEBCORELP2021.Controllers
+{
+    public class AgendamentoController1 : Controller
+    {
+        private readonly Contexto contexto;
+
+        public AgendamentoController1(Contexto context)
+        {
+            contexto = context;
+        }
+
+        public IActionResult ListarAgendamento()
+        {
+            IEnumerable<Agendamento> lstAgendamento = from agendamento in contexto.Consultas
+                                                      .Include(m => m.medico)
+                                                      .Include(p => p.paciente)
+                                                      .Include(s => s.paciente.planoDeSaude)
+                                                      .ToList()
+                                                      select new Agendamento {
+                                                          descricaoConsulta = agendamento.descricao,
+                                                          nomeMedico = agendamento.medico.nome,
+                                                          nomePaciente = agendamento.paciente.nome,
+                                                          nomePlano = agendamento.paciente.planoDeSaude.nome
+                                                           };
+            return View(lstAgendamento);
+
+        }
+
+    }
+}
---
+++ Models/Consultas/Agendamento.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WEBCORELP2021.Models.Consultas
+{
+    public class Agendamento
+    {
+
+        public string nomeMedico { get; set; } // Medico
+        public string nomePaciente { get; set; } // Paciente
+        public string nomePlano { get; set; } // Plano
+        public string descricaoConsulta { get; set; } // PlanoDeSaude
+
+    }
+}
